a2e46af3c606a7bc80602e081e82eeb1e28bbc91
castro150 <rafaelcarneiroget@hotmail.com>
2018-11-06 18:15:14 -0200

    Haroku files and changes

---
+++ web.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, '/dist')))
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+let port = process.env.PORT || 9000
+app.listen(port)
+console.log('Listening on port ' + port)
